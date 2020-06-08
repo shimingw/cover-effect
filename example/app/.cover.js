9125dfe60457e94c3417d630ab6d5da71764cb15
smw <shimingw@foxmail.com>
2020-06-08 11:47:08 +0800

    解析gitdiff

--- example/app/.cover.js
+++ example/app/.cover.js
@@ -1,4 +1,5 @@
 module.exports = {
   entry: './index.js',
   base: './example/app',
+  old:'mast'
 }
--- src/utils/gitDiff.js
+++ src/utils/gitDiff.js
@@ -1,3 +1,7 @@
-const shell = require('shelljs');
+const shell = require('shelljs')
+const diffSummary = require('./diffSummary')
+const a = 'master'
+const b = 'test'
+let text = shell.exec(`git diff ${a} ${b} --stat`).cat()
 
-console.log(shell.exec('git status').cat());
+console.log(diffSummary.parse(text).files)
