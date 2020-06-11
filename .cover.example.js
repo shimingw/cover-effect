85b3b89212319d8d2cc120a59e67ae850399e474
smw <shimingw@foxmail.com>
2020-06-11 19:17:50 +0800

    update

---
+++ .cover.example.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  oldBranch: 'test',
+  newBranch: 'master',
+  alias:{
+    '@a': path.join(__dirname, 'src', 'moduleA'),
+    '@b': path.join(__dirname, 'src', 'moduleB'),
+  }
+}
